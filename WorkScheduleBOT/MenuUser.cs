c270de02a9e4ba0a045647438ffa2419487ec3b7
teta <teta>
2021-12-21 11:53:28 +0200

    add datelist

--- WorkScheduleBOT/MenuUser.cs
+++ WorkScheduleBOT/MenuUser.cs
@@ -290,6 +290,22 @@ namespace WorkScheduleBOT
                     { }
                     user.LastMessage = "empty";
                     break;
+                case WhoFeaters:
+
+
+                    try
+                    {
+                        await client.SendTextMessageAsync(msg.Chat.Id, WhoFeaturesClass.FindUserInDate(msg.Text));
+                        await client.SendTextMessageAsync(
+                                msg.Chat.Id,
+                                $"основне меню",
+                                replyMarkup: ButtonStart()
+                                );
+                    }
+                    catch (Exception)
+                    { }
+                    user.LastMessage = "empty";
+                    break;
                 default:
                     break;
             }
@@ -458,13 +474,20 @@ namespace WorkScheduleBOT
                     break;
 
                 case WhoFeaters: /////////////////////////////// start WhoFeature
+                    //try
+                    //{
+                    //    await client.SendTextMessageAsync(
+                    //    msg.Chat.Id, "Виберіть пошук", replyMarkup: Who());
+                    //}
+                    //catch (Exception) { }
+                    //user.LastMessage = "empty";
                     try
                     {
                         await client.SendTextMessageAsync(
-                        msg.Chat.Id, "Виберіть пошук", replyMarkup: Who());
+                        msg.Chat.Id, "Виберіть пошук", replyMarkup: WhoFeaturesClass.GetAvailableListData());
                     }
                     catch (Exception) { }
-                    user.LastMessage = "empty";
+                    user.LastMessage = WhoFeaters;
                     break;
                 case WhoYesterdayDay:
                     try
@@ -545,7 +568,7 @@ namespace WorkScheduleBOT
             }
         }
         
-            public const string WhoFeaters = "хто на роботі?";
+            public const string WhoFeaters = "пошук по даті";
             public const string WhoYesterdayDay = "хто був вчора день?";
             public const string WhoYesterdayNight = "хто був вчора ніч?";
             public const string WhoTomorrowDay = "хто буде завтра день?";
--- WorkScheduleBOT/WhoFeaturesClass.cs
+++ WorkScheduleBOT/WhoFeaturesClass.cs
@@ -3,11 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace WorkScheduleBOT
 {
     class WhoFeaturesClass
     {
+        public static IReplyMarkup GetAvailableListData()
+        {
+            List<List<KeyboardButton>> l = new();
+            List<KeyboardButton> button = new();
+            button.Add(new KeyboardButton { Text = MenuUser.Exit });
+            l.Add(button);
+            List<Shift> shifts = new(Program.ListUserSchedule.FirstOrDefault().Schedule);
+            shifts.Reverse();   
+            if (Program.ListUserSchedule is not null)
+                foreach (var shift in shifts)
+                {
+                    List<KeyboardButton> buttons = new();
+                    buttons.Add(new KeyboardButton { Text = $"{shift.Date.ToShortDateString()} {shift.NameOfShift}"  });
+                    l.Add(buttons);
+                }
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = l
+            };
+        }
+        public static string FindUserInDate(string founDate)
+        {
+            string listUser = "";
+            foreach (var item in Program.ListUserSchedule)
+            {
+                foreach (var shift in item.Schedule)
+                {
+                    if (shift.Date.ToShortDateString() == founDate.Split(" ")[0] && shift.positionShift == "12" && shift.NameOfShift.Split(' ')[1] == founDate.Split(' ')[2])
+                        listUser += $"{shift.NameOfOwner}\n";
+                }
+            }
+            return listUser;
+        }
         public static string WhoToday(string inMassage)
         {
             string vs = "";
